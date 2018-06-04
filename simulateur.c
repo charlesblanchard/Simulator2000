66508a7810135ad896c6a53c35176f21d88048ec
Arkantos <timotheefroute@gmail.com>
2018-06-04 12:13:57 +0200

    Yoooooooooooooo voila l'affichage les gaaaaars ouaaaaaaais gros DJ Izz on est là

--- simulateur.c
+++ simulateur.c
@@ -23,3 +23,36 @@ void init_machine(Machine *m){
 void afficher_machine(Machine m){
     
 }
+
+void afficher_memoire(Machine m){
+	
+	prinf("ADR ");
+	for (int colonne =0; colonne<16; colonne++)
+		printf("    %x",colonne);
+	printf("\n   +");
+	for (int colonne=0; colonne<16; colonne++)	
+		printf("−−−−−");
+	printf("\n");
+	
+	
+	/*16 lignes de 16 nombres de  chiffres hexa */
+	
+	for (int ligne =0;ligne<16;ligne++) {
+		int adresse = 16*ligne;
+		printf("%02x | ",adresse);
+		for (int colonne=0; colonne<16; colonne++) {
+			printf("%04x ",m -> M[adresse + colonne]);
+		}
+		printf("\n");
+	}
+	
+	
+	/* registres */
+	
+	printf("Registres:  ACC [hex=%04 dec=%6d]  PC [%04x]  HALT [%x]\n" , m->ACC, m->ACC, m->PC, m->HALT); }
+	
+	
+	
+	
+	
+}
