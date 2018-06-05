08fa27fa8d494b3eaa5559b4e7a8dd7464c38863
Arkantos <timotheefroute@gmail.com>
2018-06-05 11:02:24 +0200

    un ptit main à la bien pour avancer

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
 #include "simulateur.h"
 #include "interpreteur.h"
 
-int main(){
+int main(int argc, char *argv[]){
     Machine *m;
     
+    char *nom_fichier[];
+    int i=0;
+    int y=0;
+    while (argv[1][i]!='.') {
+		nom_fichier[i]=argv[1][i];
+		i++;
+	  }
+    nom_fichier[i]='\0';
+    i++;
+    if (argv[1][i]==h)        /* Si le fichier est en hexa */
+    /*charger .hex */
+    else 
+    /* traduire en hexa puis charger */                   
+		
     
     return 0;
 }
