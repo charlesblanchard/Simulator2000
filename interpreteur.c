8bd2a5689443a851397ca682fff8aae58eac4db3
Thomas <thomas.hamain@gmail.com>
2018-06-06 12:42:47 +0200

    base pour interpreteur.c ARTHOURRRRR, IN - TER - PRRRREETTE

--- interpreteur.c
+++ interpreteur.c
@@ -9,6 +9,7 @@
 
 #define T_MEM 0
 #define PC_DER_LIGNE 4031713024 + T_MEM 
+#define OP_INCONNU 404
 
 
 
@@ -16,15 +17,28 @@ int interpreter(Machine *m){
     
     int32_t i=0;
     int32_t instruction = 0;
+    /*lorsqu'on aura mis en place un système d'erreurs*/
+    int erreur = 0;
     
     
     do{
-        instruction = instruction || m->RAM[i+1] << 6;
-        instruction = instruction || m->RAM[i] << 4;
-        instruction = instruction || m->RAM[i+3] << 2;
+        instruction = instruction || m->RAM[i+1] << 24;
+        instruction = instruction || m->RAM[i] << 16;
+        instruction = instruction || m->RAM[i+3] << 8;
         instruction = instruction || m->RAM[i+2];
+        
+        if (instruction >> 28 == 0xE || instruction >> 28 == 0xF){
+            
+        } else {
+            erreur = 1;
+            instruction = PC_DER_LIGNE;
+        }
         
     }while(instruction != PC_DER_LIGNE); 
     /*while pc ne revoit pas vers la derniere case*/
+    
+    if (erreur = 1){
+        printf("Opération inconnue, interpretation %d intérrompue\n",i); 
+        return OP_INCONNU;
     
 }
