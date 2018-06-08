c30f41dd4b73df75200694d1de79320199e78d44
Thomas <thomas.hamain@gmail.com>
2018-06-08 11:10:05 +0200

    mov(val,mnv(val),movt(val),movw(val) fonctionnels BITCH

--- interpreteur.c
+++ interpreteur.c
@@ -18,7 +18,7 @@
 int interpreter(Machine *M){
     
     int32_t i=0;
-    int32_t instruction = 0;
+    uint32_t instruction = 0x00000000;
     /*lorsqu'on aura mis en place un système d'erreurs*/
     int erreur = 0;
     
@@ -27,11 +27,22 @@ int interpreter(Machine *M){
      * >> n correspond à / 2^(n)
      Pour toute opération déjà notée en >> ou << il est possible de faire find replace par * pow(2,n) ou / pow(2,n), il faudra juste remplacer n par la valeur suivant << ou >> [ATTENTION]*/
     
-    do{
-        instruction = (instruction || M->RAM[i+1]) * pow(2,24);
-        instruction = (instruction || M->RAM[i]) * pow(2,16);
-        instruction = (instruction || M->RAM[i+3]) * pow(2,8);
-        instruction = (instruction || M->RAM[i+2]);
+    printf("\n");
+    
+    M->FLASH[1] = 0xF6;
+    M->FLASH[0] = 0x44;
+    M->FLASH[3] = 0x35;
+    M->FLASH[2] = 0xF5;
+    
+    do{    
+        instruction = 0;
+        
+        instruction = instruction + M->FLASH[i+1] * pow(2,24);
+        instruction = instruction + M->FLASH[i] * pow(2,16);
+        instruction = instruction + M->FLASH[i+3] * pow(2,8);
+        instruction = instruction + M->FLASH[i+2];
+        
+    printf("instruction: %x\n",instruction);
     
 
     int code = (instruction & ~(0xFE1FFFFF)) / pow(2,20); /*code d'une opération*/
@@ -41,7 +52,7 @@ int interpreter(Machine *M){
     int w = (instruction & ~(0xFBFFFFFF)) / pow(2,24); /*bit constituants la valeur paramètre de l'opération movt et movw*/
     int regd = (instruction & ~(0xFFFFF0FF)) / pow(2,8); /*bits informants du registre de déstination*/
     int val1 = (instruction & ~(0xFFFFFF0F)) / pow(2,4); /*bit informants de la valeur stockée dans la mémoire*/
-    int val2_regv = (instruction & ~(0xFFFFFF0)); /*bit informants de la valeur stockée dans la mémoire / registre stockant la valeur paramètre*/
+    int val2_regv = (instruction & ~(0xFFFFFFF0)); /*bit informants de la valeur stockée dans la mémoire / registre stockant la valeur paramètre*/
     int hex1 = (instruction & ~(0x0FFFFFFF)) / pow(2,28);
     int hex2 = (instruction & ~(0xF0FFFFFF)) / pow(2,24);
     int hex3 = (instruction & ~(0xFF0FFFFF)) / pow(2,20);
@@ -50,13 +61,10 @@ int interpreter(Machine *M){
     int hex6 = (instruction & ~(0xFFFFF0FF)) / pow(2,8);
     int hex7 = (instruction & ~(0xFFFFFF0F)) / pow(2,4);
     int hex8 = (instruction & ~(0xFFFFFFF0));
+    int zzzzwyyyxxxxxxxx = rego_z * pow(2,12) + w * pow(2,11) + xy * pow(2,8) + val1 * pow(2,4) + val2_regv;
     
     
-
-    
-    printf("code : %x\tpsr : %x\trego_z : %x\txy : %x\tw : %x\tregd : %x\tval1 : %x\tval2_regv : %x",code,psr,rego_z,xy,w,regd,val1,val2_regv);
-        
-        if (instruction / pow(2,28) == 0xE){
+        if (hex1 == 0xE){
             /*mov(reg),mvm(reg),ops(reg),décalages(val)*/
            /*****************************************/
             /*mov(reg)*/
@@ -66,19 +74,19 @@ int interpreter(Machine *M){
             
             /****************************************/
             
-        } else if (instruction / pow(2,28) == 0xF){
+        } else if (hex1 == 0xF){
             /*mov(val),movn(val),movt(val),movw(val),décalages(reg),mul(reg),ops(val)*/
             /**********************************/
             /*mov(val),movn(val)*/
-            if (hex2 == 0x0 && hex4 == 0x0){
-                if (hex3 / pow(2,1) == 2){
+            if (hex2 == 0x0 && hex4 == 0xF){
+                if (hex3 == 0x5 || hex3 == 0x4){
                     /*mov(val)*/
                     printf("mov(val)\n");
-                    mov(M, regd, (val1 * pow(2,4)) || val2_regv, psr);
-                } else if (hex3 / pow(2,1) == 0x3){
+                    mov(M, regd, (val1 * pow(2,4)) + val2_regv, psr);
+                } else if (hex3 == 0x7 || hex3 == 0x6){
                     /*mvn(val)*/
                     printf("mvn(val)\n");
-                    mvn(M, regd, (val1 * pow(2,4)) || val2_regv, psr);
+                    mvn(M, regd, (val1 * pow(2,4)) + val2_regv, psr);
                 } else {
                     erreur = 1;
                     instruction = PC_DER_LIGNE;
@@ -90,11 +98,11 @@ int interpreter(Machine *M){
                 if (hex3 == 0xC){
                     /*movt(val)*/
                     printf("movt(val)\n");
-                    movt(M, regd, (((((rego_z * pow(2,12)) || (w * pow(2,11) )) || (xy * pow(2,8))) || (val1 * pow(2,4))) || (val2_regv * pow(2,16)))); /*val = zzzzwyyy xxxxxxxx*/
+                    movt(M, regd, zzzzwyyyxxxxxxxx);
                 } else if (hex3 == 0x4){
                     /*movw(val)*/
                     printf("movw(val)\n");
-                    movw(M, regd, ((((rego_z * pow(2,12)) || (w * pow(2,11))) || (xy * pow(2,8))) || (val1 * pow(2,4))) || val2_regv); /*val = zzzzwyyy xxxxxxxx*/
+                    movw(M, regd, zzzzwyyyxxxxxxxx); /*val = zzzzwyyy xxxxxxxx*/
                 } else {
                     erreur = 1;
                     instruction = PC_DER_LIGNE;
@@ -106,7 +114,8 @@ int interpreter(Machine *M){
             instruction = PC_DER_LIGNE;
         }
         
-    }while(instruction != PC_DER_LIGNE); 
+        
+    }while(i != 0); 
     /*while pc ne revoit pas vers la derniere case*/
     
 
