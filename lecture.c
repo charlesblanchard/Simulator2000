b638aaba74761c2e5a10583a15c3cffcc224bbd8
Astrob0iii <39899156+Astrob0iii@users.noreply.github.com>
2018-06-11 10:44:38 +0200

    Update lecture.c

--- lecture.c
+++ lecture.c
@@ -20,13 +20,15 @@ void flasher_prgm(Machine *m, char fic_hexa[]){
     /* Ré-arrangement des mots de 32 bits (avec little endian) */
          
     uint16_t mot_haut, mot_bas;
-    uint32_t mot_final;
+    uint32_t mot;
     int i = 0;
     
-    while ((fscanf(f,"%" SCNx16, &mot_haut) == 1) & (fscanf(f,"%" SCNx16, &mot_bas) == 1)){
+    while ((fscanf(f,"%" SCNx32, &mot) == 1)){
+        mot_haut = (mot >> 16);
+        mot_bas = mot & (0x0000ffff);
         mot_haut = (mot_haut >> 8) | ((mot_haut &~ 0xff00) << 8);
         mot_bas = (mot_bas >> 8) | ((mot_bas &~ 0xff00) << 8);
-        mot_final = (mot_haut << 16) | mot_bas;
+        mot = (mot_haut << 16) | mot_bas;
         
         m->FLASH[i] = mot_final;
         i++;
